c2b12d41a850a2678a2688c7b5f7ec902788baf4
osamakhanedu <osamakhanedu@gmail.com>
2019-10-08 10:08:34 +0500

    Form Input Added

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,42 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img class="logo" alt="Vue logo" src="./assets/logo.png" />
+    <Skills />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Skills from "./components/Skills.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    Skills
   }
-}
+};
 </script>
 
-<style>
+<style >
+@import url("https://fonts.googleapis.com/css?family=Montserrat:400,700");
+
+body {
+  background-color: #eeeeee;
+  font-family: "Montserrat", sans-serif;
+  display: grid;
+  grid-template-rows: auto;
+  justify-items: center;
+  align-items: center;
+}
+body,
+html {
+  margin: 0;
+  height: 100%;
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 50%;
+}
+
+.logo {
+  margin-left: 30%;
 }
 </style>
---
+++ src/components/Skills.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="container">
+    <form @submit.prevent="addSkill">
+      <input type="text" placeholder="Enter a skill you have.." v-model="skill" />
+    </form>
+    <div class="holder">
+      <ul>
+        <li v-for="(data, index) in skills" :key="index">{{data.skill}}</li>
+      </ul>
+      <p>These are the skills that you possess.</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Skills",
+  data() {
+    return {
+      name: "Learning Vue",
+      // btnState: true,
+      skill: "",
+      skills: [{ skill: "Vue.js" }, { skill: "Frontend Developer" }],
+      showAlert: true
+    };
+  },
+  methods: {
+    addSkill() {
+      if (this.skill) {
+        this.skills.push({ skill: this.skill });
+        this.skill = "";
+      }
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style  scoped>
+.container {
+  box-shadow: 0px 0px 40px #61c4fd;
+}
+
+.holder {
+  background: #fff;
+  border-radius: 8px 0 0 0;
+}
+
+ul {
+  margin: 0;
+  padding: 0;
+  list-style-type: none;
+}
+
+ul li {
+  padding: 20px;
+  font-size: 1.3em;
+  background-color: #c2e4f7;
+  border-left: 5px solid #3eb3f6;
+  border-radius: 6px 0 0 6px;
+  margin-bottom: 2px;
+  color: #3e5252;
+}
+
+p {
+  text-align: center;
+  padding: 30px 0;
+  color: gray;
+}
+
+input {
+  width: calc(100% - 40px);
+  border: 0;
+  border-radius: 3px;
+  padding: 20px;
+  font-size: 1.3em;
+  background-color: #323333;
+  color: #687f7f;
+}
+
+.alert {
+  background: #f84b3e;
+  font-weight: bold;
+  color: #687f7f;
+  display: inline-block;
+  padding: 5px;
+  margin-top: -20px;
+}
+</style>
